cf730113b1c409d741438e75f97be684e68f14fd
Nikhil Kohli <nikhilkohli2407@gmail.com>
2025-04-08 09:40:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.get("/check",(req, res)=>{
 
 
 const database = async()=>{
-    const connectDB = await mongoose.connect("mongodb+srv://nikhil:nikhil@cluster0.07hcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const connectDB = await mongoose.connect("MONGO_URL");
     console.log("connected to databse")
 }
 database();
